472c792d4c6d84fbcc9d39052b94791a824dac4b
JhonatanDczel <dev.dczel@gmail.com>
2025-03-20 23:59:12 -0500

    Detalles parte 2

--- src/screens/Hymn.jsx
+++ src/screens/Hymn.jsx
@@ -3,62 +3,22 @@ import Like from '../components/Like';
 import { StyleSheet } from 'nativewind';
 import Title from '../components/Title';
 import SectionLyric from '../components/SectionLyric';
+import Himno from '../utils/CantoATi.js';
 
-const Hymn = () => {
-  const Hymno = {
-  "id": 17,
-  "title": "DEMOS GRACIAS AL SEÑOR",
-  "note": "C",
-  "author": "Desconocido",
-  "number": 17,
-  "verses": 3,
-  "lyrics": [
-    {
-      "type": "Coro",
-      "lines": [
-        "Demos gracias al Señor, demos gracias",
-        "demos gracias por su amor."
-      ]
-    },
-    {
-      "type": "Verso 1",
-      "lines": [
-        "Por la mañana las aves cantan",
-        "las alabanzas de Cristo el Salvador",
-        "y tú, amigo, ¿por qué no cantas",
-        "las alabanzas de Cristo el Salvador?"
-      ]
-    },
-    {
-      "type": "Verso 2",
-      "lines": [
-        "Y las estrellas de noche brillan",
-        "y nos alumbran por el amor de Dios",
-        "y tú amigo, mirando al cielo,",
-        "¿Por qué no alabas al Salvador Jesús?"
-      ]
-    },
-    {
-      "type": "Verso 3",
-      "lines": [
-        "En el calvario murió inocente",
-        "por tus pecados clavado en una cruz",
-        "y si tú quieres la vida eterna",
-        "¿Por qué no acudes al Salvador Jesús?"
-      ]
-    }
-  ]
-}
+const Hymn = ({himno}) => {
+
+  //Proction
+  himno = Himno;
 
   return (
     <View className='w-full h-full flex-col justify-start'>
       <View className="header w-full flex gap-0.5 pt-10 pb-3 px-8 bg-UIbase" style={[styles.shadowProp]}>
-        <Title title="Canto a ti"></Title>
+        <Title title={himno.title}></Title>
         <View className='topBar flex-row justify-between items-center'>
           <Text className='font-josefin text-base'>
-            Himno 3 • 5 verssos
+            Himno {himno.number} • {himno.verses} versos
           </Text>
-          <Text className='font-josefin text-base'>F#m</Text>
+          <Text className='font-josefin text-base'>{himno.key}</Text>
           <Like/>
         </View>
       </View>
@@ -66,7 +26,7 @@ const Hymn = () => {
         <View className='lyrics flex-col gap-5'>
 
         {
-          Hymno.lyrics.map((lyric, index) => (
+          himno.lyrics.map((lyric, index) => (
             <SectionLyric lyrics={lyric}/>
           ))
         }
---
+++ src/utils/CantoATi.js
@@ -0,0 +1,46 @@
+  const Himno = {
+  "id": 17,
+  "title": "DEMOS GRACIAS AL SEÑOR",
+  "note": "C",
+  "author": "Desconocido",
+  "number": 17,
+  "verses": 3,
+  "lyrics": [
+    {
+      "type": "Coro",
+      "lines": [
+        "Demos gracias al Señor, demos gracias",
+        "demos gracias por su amor."
+      ]
+    },
+    {
+      "type": "Verso 1",
+      "lines": [
+        "Por la mañana las aves cantan",
+        "las alabanzas de Cristo el Salvador",
+        "y tú, amigo, ¿por qué no cantas",
+        "las alabanzas de Cristo el Salvador?"
+      ]
+    },
+    {
+      "type": "Verso 2",
+      "lines": [
+        "Y las estrellas de noche brillan",
+        "y nos alumbran por el amor de Dios",
+        "y tú amigo, mirando al cielo,",
+        "¿Por qué no alabas al Salvador Jesús?"
+      ]
+    },
+    {
+      "type": "Verso 3",
+      "lines": [
+        "En el calvario murió inocente",
+        "por tus pecados clavado en una cruz",
+        "y si tú quieres la vida eterna",
+        "¿Por qué no acudes al Salvador Jesús?"
+      ]
+    }
+  ]
+}
+
+export default Himno;
