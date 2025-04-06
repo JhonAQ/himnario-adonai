90e8438fe9a7b37a082474dda0845b29f3146335
JhonatanDczel <dev.dczel@gmail.com>
2025-04-06 00:00:39 -0500

    Mejorando el script para visualizar himnos

--- scripts/view-hymn.js
+++ scripts/view-hymn.js
@@ -1,11 +1,16 @@
-const {readFile}  = require('fs');
-const {OpenLyricsParser} = require('openlyrics-parser');
+const path = require('path');
+const { readFile } = require('fs');
+const { OpenLyricsParser } = require('openlyrics-parser');
+const util = require('util');
 
 const hymnFile = process.argv[2];
 let song = null;
 
-readFile(hymnFile, 'utf8', (err, data) => {
+const initialCwd = process.env.INIT_CWD || process.cwd();
+const filePath = path.resolve(initialCwd, hymnFile);
+
+readFile(filePath, 'utf8', (err, data) => {
     if (err) throw err;
     song = OpenLyricsParser(data.toString());
-    console.log(song);
-})
+    console.log(util.inspect(song, { depth: null, colors: true }));
+});
