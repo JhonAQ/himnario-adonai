df6168c15f942c4c5f5bc7c0d2cce6af16286759
JhonatanDczel <dev.dczel@gmail.com>
2025-04-10 22:17:55 -0500

    Hymns correction in progress.. this commit its automatically generated

--- scripts/commit-hymns.sh
+++ scripts/commit-hymns.sh
@@ -1,2 +1,7 @@
+if [ -z "$(git status --porcelain)" ]; then
+  echo "No hay cambios para commitear."
+  exit 0
+fi
+
 git add -A
 git commit -m "Hymns correction in progress.. this commit its automatically generated"
