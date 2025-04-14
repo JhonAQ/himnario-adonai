40e591b8fa8279db5840cf06564ba9f1be165165
JhonatanDczel <dev.dczel@gmail.com>
2025-04-14 16:46:15 -0500

    parse hymn script

--- scripts/view-hymn.js
+++ scripts/view-hymn.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const { readFile } = require('fs');
+const { readFile, writeFile } = require('fs');
 const { OpenLyricsParser } = require('openlyrics-parser');
 const util = require('util');
 
@@ -8,9 +8,16 @@ let song = null;
 
 const initialCwd = process.env.INIT_CWD || process.cwd();
 const filePath = path.resolve(initialCwd, hymnFile);
+const outputPath = path.resolve(initialCwd, 'parsed_song.json');
 
 readFile(filePath, 'utf8', (err, data) => {
     if (err) throw err;
     song = OpenLyricsParser(data.toString());
+
+    writeFile(outputPath, JSON.stringify(song, null, 2), 'utf8', (err) => {
+        if (err) throw err;
+        console.log(`Archivo guardado en: ${outputPath}`);
+    });
+
     console.log(util.inspect(song, { depth: null, colors: true }));
 });
