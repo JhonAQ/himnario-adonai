0ec28cfcf5bd944dca01bb6e51f6a929dff31ab7
JhonatanDczel <jariasq@unsa.edu.pe>
2025-04-25 23:29:47 -0500

    Agrega categorias

--- src/context/HimnosContext.js
+++ src/context/HimnosContext.js
@@ -5,6 +5,23 @@ export const HimnosContext = createContext();
 
 export const HimnosProvider = ({ children }) => {
   const [metaHimnos, setMetaHimnos] = useState(null);
+  const categories = [
+    'Adoración y Alabanza',
+    'Espíritu Santo',
+    'Vida de Cristo',
+    'Iglesia y Comunidad',
+    'Arrepentimiento y Confesión',
+    'Esperanza y Segunda Venida',
+    'Bautismo y Santa Cena',
+    'Oración y Devoción Personal',
+    'Navidad y Pascua',
+    'Funerales y Consuelo',
+    'Niños y Escuela Dominical',
+    'Misión y Evangelismo',
+    'Consagración y Servicio',
+    'Coros'
+  ]
+
   const [recentlyViewed, setRecentlyViewed] = useState([])
 
   useEffect(() => {
